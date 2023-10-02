8fd6916a178d7ff5bb6410fe34a22f4e0218e4a7
Karl Gavillot <ksgavillot@hotmail.com>
2023-10-02 15:07:38 +0200

    Section 7 - 95 Writing the Second Test

--- Calculator/src/main/com/tuto/calculator/Calculator.java
+++ Calculator/src/main/com/tuto/calculator/Calculator.java
@@ -2,6 +2,6 @@ package com.tuto.calculator;
 
 public class Calculator {
     public int add(int num1, int num2) {
-        return 0;
+        return num1 + num2;
     }
 }
--- Calculator/src/test/com/tuto/calculator/CalculatorTest.java
+++ Calculator/src/test/com/tuto/calculator/CalculatorTest.java
@@ -1,18 +1,39 @@
 package com.tuto.calculator;
 
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions; // j'ai utilisé https://start.spring.io/ pour générer pom.xml | pb import Junit depuis ProjectStructure => assertEquals
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Section 7 Testing Code - 94 Our First Test
+ *
+ * RQ assertEquals() peut prendre un 3em arg = message
+ * cela s'avère pratique, en particulier dans les cas où vous pouvez avoir plusieurs assertions dans un seul test méthode
+ * et vous souhaitez identifier laquelle des multiples assertions de cette méthode a été en échec
+ */
 public class CalculatorTest {
 
+    private Calculator calc;
+
+    @BeforeEach // Annotation va s'executé juste avant @Test
+    void setUp() {  // clic D calc / Refactor / Introduce field / setup
+        calc = new Calculator(); // Crée une instance de la Class Calculator pour chaque test mais sur le meme field = Déférence la 1ere address pour pointer vers la 2em => Evite que cette variable stocke des valeurs (states) qui pourrait affecter le test suivant
+    }
+
+    @Test // Framework JUnit scanne le code à la recherche de toutes les methodes annotés => Junit sauras qu'elles méthodes elle doit appeler pour exécuter nos tests
+    public void canAddZeroPlusZero() {
+        int sum = calc.add(0,0); // depuis instance calc lance la methode add() et stock le return dans la variable sum
+        Assertions.assertEquals(0, sum); // assertEquals => 1er arg = ce que j'attend comme value comparé avec 2em arg = la valeur de la variable qui stock le return de la methode add() de l'instance calc de la class Calculator => si égal true si non false | peut prendre un 3em arg commentaire
+    }
+
     @Test
-    public void canZeroPlusZero() {
-        Calculator calc = new Calculator();
-        int sum = calc.add(0,0);
-        Assertions.assertEquals(0, sum);
+    public void canAddOnePlusOne() {
+        // Calculator calc = new Calculator(); // instance de la class Calculator
+        int sum = calc.add(1,1);
+        Assertions.assertEquals(2, sum);
     }
 
 
