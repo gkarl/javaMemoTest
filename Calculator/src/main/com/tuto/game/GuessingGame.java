320fea014519dc931487a3c24f1ceeea256a7366
Karl Gavillot <ksgavillot@hotmail.com>
2023-10-04 15:02:32 +0200

    Section 7 - 102 Tracking the Number of Guesses

--- Calculator/src/main/com/tuto/game/GuessingGame.java
+++ Calculator/src/main/com/tuto/game/GuessingGame.java
@@ -4,16 +4,22 @@ import java.util.Random;
 
 public class GuessingGame {
 
-    private final int randomNumber = new Random().nextInt(10) + 1; // Comme ça le nbre random n'est généré qu'une fois et la methode guess() le nbre peut etre trouvé | autre solution le mettre dans un constructeur pour que ce nbre soit crée à la création d'une instance
+    private final int randomNumber = new Random().nextInt(10) + 1; //Lesson 100 ** Comme ça le nbre random n'est généré qu'une fois et la methode guess() le nbre peut etre trouvé | autre solution le mettre dans un constructeur pour que ce nbre soit crée à la création d'une instance
+    private int counter = 0; // Lesson 102 **
 
-    //_____________________Lesson 98 - Reimplementing the Guessing Game with TDD___________________________
     // Avec la methodologie TDD (test driven design) nous essayons de ne pas écrire plus de code réel que ce dont nous avons absolument besoin pour que le test passe.
     public String guess(int guessedNumber) {
+        counter++;  // counte le nbre de tentatives
+        if (counter == 4 && guessedNumber != getRandomNumber()) { // Lesson 102 ** 4 est le nbre d'essais max Game Over
+            return "Vous n'avez pas trouvé le bon chiffre et vous c'est votre 4em essaie. Game over";
+        }
+        String trytext = counter == 1 ? "try" : "tries";
+        String winningMsg = String.format("Vous avez trouvé le bon chiffre in %d %s", counter, trytext);
         // return guessedNumber >= 0 ? "Vous avez trouvé le bon chiffre" : "Vous n'avez pas trouvé le bon chiffre"; // methode simple pour que le test testOneWrongNegGuessSituation passe
-        return guessedNumber == getRandomNumber() ? "Vous avez trouvé le bon chiffre" : "Vous n'avez pas trouvé le bon chiffre"; // methode simple pour que le test testOneWrongGuessSituation + testOneWrongPosGuessSituation passe | le test 1 ne passe plus car on cherche une égalité sur 2 random number => solution créer 1 field qui va contenir la génération de nobre random
+        return guessedNumber == getRandomNumber() ? winningMsg : "Vous n'avez pas trouvé le bon chiffre"; // Lesson 99 ** methode simple pour que le test testOneWrongGuessSituation + testOneWrongPosGuessSituation passe | le test 1 ne passe plus car on cherche une égalité sur 2 random number => solution créer 1 field qui va contenir la génération de nobre random
     }
 
-    //_____________________Lesson 100 - Implementing Randomness___________________________
+
     public int getRandomNumber() {
         // return new Random().nextInt(10) + 1; // nextInt() => peut prendre en arg une  borne supérieure (bound) ex 10 [0-9] +1 => [1-10]
         return randomNumber;
--- Calculator/src/test/com/tuto/game/GuessingGameTest.java
+++ Calculator/src/test/com/tuto/game/GuessingGameTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class GuessingGameTest {
 
+    public static final int GAME_RANDOMNESS_RETRIES = 100;
     private GuessingGame game;
 
     @BeforeEach // Annotation va s'executé juste avant chaque @Test
@@ -21,7 +22,7 @@ public class GuessingGameTest {
     public void testSimpleWinSituation(){ // test le cas ou user trouve le nbre
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum); // Imagine que l'user devine (guess) le random number | guess() => affiche un message si nbre trouvé ou pas
-        Assertions.assertEquals("Vous avez trouvé le bon chiffre", message); // Test si la methode guess() return bien "Vous avez trouvé le bon chiffre"
+        Assertions.assertEquals("Vous avez trouvé le bon chiffre in 1 try", message); // Test si la methode guess() return bien "Vous avez trouvé le bon chiffre"
     }
 
     //_____________________Lesson 99 - Guessing Game TDD Part Two___________________________
@@ -44,7 +45,7 @@ public class GuessingGameTest {
     @Test
     public void testRandomNumberGeneration(){ // test si notre methode getRandomNumber() est vraiment efficace et génére un nbre aléatoire égalemnt distribué
         int[] rndNumCount = new int[11]; // On crée un Array avec 10 élément de type int => il met 11 au final car si non la 2em boucle for nous crée un outbound
-        for (int counter=0; counter < 50; counter++) { // on va bouclé 100 fois | ** grace à l'annotation @RepeatTest(10) à la place de @Test => ça répéte le test 10 fois pour voire si il passe à chaque fois il a pus déterminer que bouclé 50 fois suffisait pour que tous les éléments de l'arrays soit à 1
+        for (int counter = 0; counter < GAME_RANDOMNESS_RETRIES; counter++) { // on va bouclé 100 fois | ** grace à l'annotation @RepeatTest(10) à la place de @Test => ça répéte le test 10 fois pour voire si il passe à chaque fois il a pus déterminer que bouclé 50 fois suffisait pour que tous les éléments de l'arrays soit à 1
             GuessingGame game = new GuessingGame(); //** Pour que le test passe on crée une instance que pour ce test => recrée un random nbre à chaque tour de boucle | si non on aurait eu tjrs le meme random nbre du fait qu'on l'ai mis en field dans la class GuessingGame
             int randomNum = game.getRandomNumber();  // sur la methode getRandomNumber() et on va stocker sa valeur dans la variable randomNumrndNumCounrndNumCount
             rndNumCount[randomNum] = 1; // getRandomNumber() génére un nbre de 1 à 10 qui correspond à l'index de notre Array rndNumCount => qd un nbre est sorti par notre moteur de random il lui assigne la valeur 1 correspondant dans notre Array
@@ -56,5 +57,33 @@ public class GuessingGameTest {
         Assertions.assertEquals(10, sum); // il s'attend à avoir 10 from sum => si c'est le cas getRandomNumber() a un bon moteur de random les chiffre sont produit de façon équivalente car tous les flafg de notre Array on été allumé aprés avoir lancé la methode 100 fois
     }
 
+    //_____________________Lesson 102 Tracking the Number of Guesses___________________________
+    @Test
+    public void testFourWrongGuesses() { // test si atteind le max de 4 tentatives échouent game over
+        game.guess(-3);
+        game.guess(-3);
+        game.guess(-3);
+        String message = game.guess(-3);
+        Assertions.assertEquals("Vous n'avez pas trouvé le bon chiffre et vous c'est votre 4em essaie. Game over", message);
+    }
+
+    @Test
+    public void testTreeWrongGuessesAndOneCorrect() { // test si fait 3 tentatives échouent et 1 réussit
+        game.guess(-3);
+        game.guess(-3);
+        game.guess(-3);
+        int correctAnswer = game.getRandomNumber();
+        String message = game.guess(correctAnswer);
+        Assertions.assertEquals("Vous avez trouvé le bon chiffre in 4 tries", message); // il rajoute de tester dans la methode guess() si retourne bien le nbre de tentatives mais aussi que le message affiche try qd c'est 1 essaye et tries qd plusieurs essais
+    }
+
+    @Test
+    public void testTowWrongGuessesAndOneCorrect() { // test si fait 2 tentatives échouent et 1 réussit
+        game.guess(-3);
+        game.guess(-3);
+        int correctAnswer = game.getRandomNumber();
+        String message = game.guess(correctAnswer);
+        Assertions.assertEquals("Vous avez trouvé le bon chiffre in 3 tries", message);
+    }
 
 }
